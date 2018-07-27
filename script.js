e40de8e4e62025ca3e94c90545b6bb5cb41096af
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2018-07-27 16:47:17 -0600

    secondary commit

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+/**  BMI challange
+ **/
+
+
+/** var markMass, markHeight, markBMI, johnHeight, johnMass, johnBMI, markHigherBMI;
+
+markMass = 150;
+markHeight = 2.5;
+//console.log(markHeight)
+johnHeight = 2;
+johnMass = 250;
+
+markBMI = markMass / (markHeight * markHeight);
+console.log(markBMI)
+
+
+johnBMI = johnMass / (johnHeight * johnHeight);
+console.log(johnBMI);
+
+
+markHigherBMI = markBMI > johnBMI
+console.log(markHigherBMI);
+**/
+
 var firstName = 'Jason';
-console.log(firstName)
-
-var lastName = "Goldfarb";
-console.log(lastName)
-
-
 var age = 38;
-console.log(age)
+
+age >= 18 ? console.log(firstName = 'drinks beer.') : console.log(firstName = 'drinks juice.');
